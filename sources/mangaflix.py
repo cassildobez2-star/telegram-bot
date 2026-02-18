1c31352e8f9b842d49175dcc7096c16c8fffaf6d
cassildobez2-star <cassildobez2@gmail.com>
2026-02-18 15:48:09 +0200

    Update mangaflix.py

--- sources/mangaflix.py
+++ sources/mangaflix.py
@@ -1,63 +1,47 @@
 # sources/mangaflix.py
-import aiohttp
+import requests
 
 class MangaFlixSource:
     BASE_URL = "https://api.mangaflix.net/v1"
 
-    def __init__(self):
-        self.session = None
-
-    async def _get_session(self):
-        if not self.session:
-            self.session = aiohttp.ClientSession()
-        return self.session
-
-    async def search(self, query: str):
-        session = await self._get_session()
+    def search(self, query: str):
         url = f"{self.BASE_URL}/search/mangas?query={query}&selected_language=pt-br"
         try:
-            async with session.get(url) as resp:
-                data = await resp.json()
-                results = []
-                for item in data.get("data", []):
-                    results.append({
-                        "title": item.get("name"),
-                        "url": f"/br/manga/{item.get('_id')}"
-                    })
-                return results
+            resp = requests.get(url, timeout=10)
+            data = resp.json()
+            results = []
+            for item in data.get("data", []):
+                results.append({
+                    "title": item.get("name"),
+                    "url": f"/br/manga/{item.get('_id')}"
+                })
+            return results
         except Exception:
             return []
 
-    async def chapters(self, manga_id: str):
-        session = await self._get_session()
+    def chapters(self, manga_id: str):
         mid = manga_id.split("/")[-1]
         url = f"{self.BASE_URL}/mangas/{mid}"
         try:
-            async with session.get(url) as resp:
-                data = await resp.json()
-                chapters = []
-                for ch in data.get("data", {}).get("chapters", []):
-                    chapters.append({
-                        "chapter_number": ch.get("number"),
-                        "url": f"/br/manga/{ch.get('_id')}",
-                        "manga_title": data.get("data", {}).get("name", "Manga")
-                    })
-                return chapters
+            resp = requests.get(url, timeout=10)
+            data = resp.json()
+            chapters = []
+            for ch in data.get("data", {}).get("chapters", []):
+                chapters.append({
+                    "chapter_number": ch.get("number"),
+                    "url": f"/br/manga/{ch.get('_id')}",
+                    "manga_title": data.get("data", {}).get("name", "Manga")
+                })
+            return chapters
         except Exception:
             return []
 
-    async def pages(self, chapter_id: str):
-        session = await self._get_session()
+    def pages(self, chapter_id: str):
         cid = chapter_id.split("/")[-1]
         url = f"{self.BASE_URL}/chapters/{cid}?selected_language=pt-br"
         try:
-            async with session.get(url) as resp:
-                data = await resp.json()
-                return [img.get("default_url") for img in data.get("data", {}).get("images", [])]
+            resp = requests.get(url, timeout=10)
+            data = resp.json()
+            return [img.get("default_url") for img in data.get("data", {}).get("images", [])]
         except Exception:
             return []
-
-    async def close(self):
-        if self.session:
-            await self.session.close()
-            self.session = None
