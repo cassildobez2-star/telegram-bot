cd3873d85e06aaf986311ef49fa194b9b164bc82
cassildobez2-star <cassildobez2@gmail.com>
2026-02-23 23:23:33 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,12 +1,16 @@
+# utils/loader.py
+
 from sources.toonbr import ToonBrSource
 from sources.mangaflix import MangaFlixSource
 from sources.mangalivreblog import MangaLivreBlogSource
+from sources.wolftoon import WolftoonSource  # nova fonte
 
 # Dicionário de fontes disponíveis
 _sources = {
     "ToonBr": ToonBrSource(),
     "MangaFlix": MangaFlixSource(),
-    "MangaLivreBlog": MangaLivreBlogSource()
+    "MangaLivreBlog": MangaLivreBlogSource(),
+    "Wolftoon": WolftoonSource(),  # adicionada
 }
 
 def get_all_sources():
