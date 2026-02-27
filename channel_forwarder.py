c8deb2aac03b6341a385f3a84dffa382e8084c6f
cassildobez2-star <cassildobez2@gmail.com>
2026-02-27 21:05:58 +0200

    Update channel_forwarder.py

--- channel_forwarder.py
+++ channel_forwarder.py
@@ -1,8 +1,8 @@
-from config import LOG_CHANNEL_ID
+from config import LOG_CHANNEL
 
 async def forward_from_channel(bot, target_chat_id, message_id):
     await bot.copy_message(
         chat_id=target_chat_id,
-        from_chat_id=LOG_CHANNEL_ID,
+        from_chat_id=LOG_CHANNEL,
         message_id=message_id
     )
