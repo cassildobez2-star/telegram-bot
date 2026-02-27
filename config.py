918d4fc5e9a58e1bebe2041fc5e4da7e9a145c94
cassildobez2-star <cassildobez2@gmail.com>
2026-02-27 20:11:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 import os
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
