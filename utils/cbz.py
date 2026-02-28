35f2588286cae8266efcb2c9bda8273202708377
cassildobez2-star <cassildobez2@gmail.com>
2026-02-28 20:08:25 +0200

    Update cbz.py

--- utils/cbz.py
+++ utils/cbz.py
@@ -1,28 +1,50 @@
 import io
 import zipfile
-import httpx
-from utils.downloader import download_image
-from utils.task_manager import is_cancelled
+import aiohttp
+from worker import CANCEL_FLAGS
 
-async def create_cbz(source, chapter, user_id):
+async def stream_zip_and_send(application, task):
+    bot = application.bot
+    chat_id = task["chat_id"]
+    user_id = task["user_id"]
+    chapters = task["chapters"]
+    source = task["source"]
+    title = task["title"]
+
+    status = await bot.send_message(chat_id, "📦 Criando ZIP...")
+
     buffer = io.BytesIO()
 
-    async with httpx.AsyncClient() as client:
-        with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for chapter in chapters:
+            if CANCEL_FLAGS.get(user_id):
+                await status.edit_text("❌ Cancelado.")
+                return
 
             pages = await source.pages(chapter["url"])
 
-            for index, img_url in enumerate(pages):
+            for i, page_url in enumerate(pages):
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(page_url) as resp:
+                        data = await resp.read()
 
-                if is_cancelled(user_id):
-                    return None
+                zipf.writestr(
+                    f"Cap_{chapter['chapter_number']}/{i}.jpg",
+                    data
+                )
 
-                img = await download_image(client, img_url)
-
-                if img:
-                    zipf.writestr(f"{index:03}.jpg", img)
-
-                del img
+            await status.edit_text(
+                f"📦 Processando Cap {chapter['chapter_number']}"
+            )
 
     buffer.seek(0)
-    return buffer
+
+    await status.edit_text("⬆️ Enviando...")
+
+    await bot.send_document(
+        chat_id,
+        document=buffer,
+        filename=f"{title}.zip"
+    )
+
+    await status.edit_text("✅ Enviado com sucesso!")
