e516b4d99056e1e2d4d866fbaaeca9707a230a81
cassildobez2-star <cassildobez2@gmail.com>
2026-02-28 23:08:33 +0200

    Delete channel_forwarder.py

--- channel_forwarder.py
+++
@@ -1,8 +0,0 @@
-from config import LOG_CHANNEL
-
-async def forward_from_channel(bot, target_chat_id, message_id):
-    await bot.copy_message(
-        chat_id=target_chat_id,
-        from_chat_id=LOG_CHANNEL,
-        message_id=message_id
-    )
