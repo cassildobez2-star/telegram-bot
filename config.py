45514e801d7896350542d5a6bd0f2f2edd8d3ffd
cassildobez2-star <cassildobez2@gmail.com>
2026-02-28 09:02:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,3 @@
 import os
 
-def get_env(name, default=None, required=True):
-    value = os.getenv(name, default)
-    if required and not value:
-        raise RuntimeError(f"Variável de ambiente obrigatória não definida: {name}")
-    return value
-
-
-API_ID = int(get_env("API_ID"))
-API_HASH = get_env("API_HASH")
-BOT_TOKEN = get_env("BOT_TOKEN")
-
-MAX_RETRIES = int(get_env("MAX_RETRIES", 3, required=False))
-HTTP_TIMEOUT = int(get_env("HTTP_TIMEOUT", 20, required=False))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
